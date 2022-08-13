8ebb1f93240802ab610d65fa9c2f0bef352a57ed
plumppiggy <elysiadarbourne@yahoo.ca>
2022-08-13 15:58:25 -0400

    added scrolling mech to the handbook page

--- pages/firstYearHandbook.js
+++ pages/firstYearHandbook.js
@@ -1,12 +1,18 @@
 import NavBar from "../components/NavBar";
+import React from 'react';
 
 import styles from "../styles/firstYearHandbook.module.css";
 
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import { useRef } from 'react';
+
+
 
 const firstYearHandbook = () => {
+  const introduction = useRef(0);
   return (
     <>
       <NavBar />
@@ -29,7 +35,17 @@ const firstYearHandbook = () => {
                   placeholder="search for a topic..."
                 />
               </form> */}
-              <a className={styles.handbookLink} href="">
+              <Nav>
+                <Nav.Item>
+                  <Nav.Link onClick={() => scrollDown(introduction)}>Introduction</Nav.Link>
+                  <NavDropdown title="First Year CheckList" autoClose={false}>
+                    <NavDropdown.Item>Preparing for Classes</NavDropdown.Item>
+                    <NavDropdown.Item>Remote Learning Tips</NavDropdown.Item>
+                    <NavDropdown.Item>Off Campus Housing</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav.Item>
+              </Nav>
+              {/* <a className={styles.handbookLink} href="">
                 Introduction
               </a>
               <a className={styles.handbookLink} href="">
@@ -52,18 +68,29 @@ const firstYearHandbook = () => {
               </a>
               <a className={styles.handbookLink} href="">
                 Additional Information
-              </a>
+              </a> */}
             </div>
           </Col>
           <Col>
             <h1>Math Handbook</h1>
-            <p>Introduction</p>
-            <p>Message from the math o-team...</p>
+            <div className="section" ref={introduction}>
+              <p>Introduction</p>
+              <p>Message from the math o-team...</p>
+            </div>
           </Col>
         </Row>
       </Container>
     </>
   );
 };
+
+const scrollDown = (ref) => {
+  window.scrollTo({
+    top: ref.current.offsetTop,
+    behaviour: 'smooth',
+  });
+};
+
+
 
 export default firstYearHandbook;
