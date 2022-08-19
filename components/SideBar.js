a46136405e3ad4f6e16fe933ee87e35d07475102
Tavares913 <jakeknocker74@gmail.com>
2022-08-19 10:55:40 -0400

    added some styles to the sidenav

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,52 +1,73 @@
-import { Container } from 'react-bootstrap';
-import SidebarMenu from 'react-bootstrap-sidebar-menu';
+import { Container } from "react-bootstrap";
+import SidebarMenu from "react-bootstrap-sidebar-menu";
 
-import sections from '../public/Handbook/sections';
+import styles from "../styles/sideBarMenu.module.css";
+
+import sections from "../public/Handbook/sections";
 
 const SideBar = () => {
-    return (
-        <div className='sidebar'>
-        <SidebarMenu expand="lg" hide="md">
+  return (
+    <SidebarMenu expand="lg" hide="md">
+      <div className={styles.sidebar}>
         <Container className="container-fluid">
-            <SidebarMenu.Header>
-            <SidebarMenu.Toggle aria-controls="basic-navbar-nav"/>
-            </SidebarMenu.Header>
-        <SidebarMenu.Collapse>
+          <SidebarMenu.Header>
+            <SidebarMenu.Toggle aria-controls="basic-navbar-nav" />
+          </SidebarMenu.Header>
+          <SidebarMenu.Collapse>
             <SidebarMenu.Body>
-                {sections.map(
-                    (section) => {
+              {sections.map((section) => {
+                return (
+                  <SidebarMenu.Sub>
+                    <SidebarMenu.Sub.Toggle
+                      style={{
+                        width: "100%",
+                        borderRadius: "5px",
+                        backgroundColor: "#facaf0",
+                        height: "2.7rem",
+                      }}
+                    >
+                      <SidebarMenu.Nav.Title>
+                        {section.header}
+                      </SidebarMenu.Nav.Title>
+                    </SidebarMenu.Sub.Toggle>
+                    <SidebarMenu.Sub.Collapse>
+                      {section.subsections.map((sub) => {
                         return (
-                            <SidebarMenu.Sub>
-                                <SidebarMenu.Sub.Toggle>
-                                    <SidebarMenu.Nav.Title>
-                                    {section.header}
-                                    </SidebarMenu.Nav.Title>
-                                </SidebarMenu.Sub.Toggle>
-                                <SidebarMenu.Sub.Collapse>
-                                    {section.subsections.map(
-                                        (sub) => {
-                                            return (
-                                                <SidebarMenu.Nav>
-                                                  <SidebarMenu.Nav.Link href={`#${sub.link}`}>
-                                                    <SidebarMenu.Nav.Title>
-                                                      {sub.title} 
-                                                    </SidebarMenu.Nav.Title>
-                                                  </SidebarMenu.Nav.Link>
-                                                </SidebarMenu.Nav>
-                                            )
-                                        }
-                                    )}
-                                </SidebarMenu.Sub.Collapse>
-                            </SidebarMenu.Sub>
-                        )
-                    }
-                )}
+                          <SidebarMenu.Nav
+                            style={{
+                              height: "2rem",
+                              display: "flex",
+                              justifyContent: "center",
+                            }}
+                          >
+                            <SidebarMenu.Nav.Link
+                              href={`#${sub.link}`}
+                              style={{
+                                color: "black",
+                                width: "100%",
+                                border: "1px solid black",
+                                borderRadius: "5px",
+                                backgroundColor: "#f7d5f0",
+                                textAlign: "center",
+                              }}
+                            >
+                              <SidebarMenu.Nav.Title>
+                                {sub.title}
+                              </SidebarMenu.Nav.Title>
+                            </SidebarMenu.Nav.Link>
+                          </SidebarMenu.Nav>
+                        );
+                      })}
+                    </SidebarMenu.Sub.Collapse>
+                  </SidebarMenu.Sub>
+                );
+              })}
             </SidebarMenu.Body>
-            </SidebarMenu.Collapse>
+          </SidebarMenu.Collapse>
         </Container>
-        </SidebarMenu>
-        </div>
-    )
-}
+      </div>
+    </SidebarMenu>
+  );
+};
 
 export default SideBar;
